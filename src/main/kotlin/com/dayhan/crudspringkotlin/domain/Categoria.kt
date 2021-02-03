1ace0ba34605c808650609311a3e69c6113ba8ff
Dayhan Henrique <gft.dlima@bv.com.br>
2021-02-03 19:02:52 -0300

    Banco de dados H2 e criacao automatica da base de dados

--- src/main/kotlin/com/dayhan/crudspringkotlin/domain/Categoria.kt
+++ src/main/kotlin/com/dayhan/crudspringkotlin/domain/Categoria.kt
@@ -1,3 +1,13 @@
 package com.dayhan.crudspringkotlin.domain
 
-data class Categoria(val id: Int, val nome: String)
+import javax.persistence.Entity
+import javax.persistence.GeneratedValue
+import javax.persistence.GenerationType
+import javax.persistence.Id
+
+@Entity
+data class Categoria(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Int,
+    val nome: String)
